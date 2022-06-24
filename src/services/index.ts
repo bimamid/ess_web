f9e7d2d2fdf4dc3f244bae0337e3ed11101aeb63
tigasatudev <tigasatudev@gmail.com>
2022-06-24 15:02:02 +0700

    update port

--- src/services/index.ts
+++ src/services/index.ts
@@ -6,8 +6,8 @@ import Helper from '@/utils/helper';
 const http = axios.create({
   baseURL:
     process.env.NODE_ENV === 'production'
-      ? 'http://139.0.5.104:3000/api'
-      : 'http://localhost:3000/api',
+      ? 'http://139.0.5.104:4000/api'
+      : 'http://localhost:4000/api',
   headers: {
     'X-Requested-With': 'XMLHttpRequest',
   },
